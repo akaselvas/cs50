7bb12ca8a3d104261644c447c7f4d95a4c5a0ca0
fabio <fabio@fabios-MacBook-Pro.local>
2024-09-16 21:05:36 -0300

    Atualizando o arquivo app.py com novas funcionalidades

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Olá, mundo! Este é meu site em Flask!</h1>"
+    return "<h1> </h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
