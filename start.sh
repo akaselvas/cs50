0c6383286f23dc5bc8d8e17ef16483b9a79cd915
selvas <91513384+akaselvas@users.noreply.github.com>
2024-10-24 00:04:23 -0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Monkey-patch
-exec python -c "import gevent.monkey; gevent.monkey.patch_all(ssl=True); import app" #  <-- **FIX APP IMPORT PATH HERE**
-
-# Run gunicorn with error logging
-exec gunicorn -k gevent app:app -b 0.0.0.0:$PORT --error-logfile -  # <-- **FIX APP:APP IF NEEDED**
+exec gunicorn -k gevent --error-logfile - --bind 0.0.0.0:$PORT "app:app" 
