0f0ee76808ce6a1b31e03b5a2687fb9a6496d1af
selvas <91513384+akaselvas@users.noreply.github.com>
2024-10-23 23:57:28 -0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
+# Monkey-patch
+exec python -c "import gevent.monkey; gevent.monkey.patch_all(ssl=True); import app" #  <-- **FIX APP IMPORT PATH HERE**
 
-# Monkey-patch BEFORE anything imports ssl (including gunicorn and your app)
-exec python -c "import gevent.monkey; gevent.monkey.patch_all(ssl=True); import app" # Import app *after* patching, so all subsequent imports are also patched
-
-# Now run gunicorn in the patched environment – it inherits the patched environment due to `exec`
-exec gunicorn -k gevent app:app -b 0.0.0.0:$PORT  # or -b :$PORT
+# Run gunicorn with error logging
+exec gunicorn -k gevent app:app -b 0.0.0.0:$PORT --error-logfile -  # <-- **FIX APP:APP IF NEEDED**
