11435e8603eff69966656afd84b374ec6734dd82
selvas <91513384+akaselvas@users.noreply.github.com>
2024-11-01 19:31:18 -0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-exec gunicorn -k gevent --error-logfile - --bind 0.0.0.0:$PORT "app:app" 
+
+# Monkey-patch before importing other modules that use sockets
+gevent monkey -v -p
+
+# Now run Gunicorn
+exec gunicorn -k gevent --worker-class socketio.sgunicorn.GeventSocketIOWorker --bind 0.0.0.0:$PORT "app:app"
