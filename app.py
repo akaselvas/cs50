4379a0fcaa9f6de12099af0eea3b0b597fbdcf13
selvas <91513384+akaselvas@users.noreply.github.com>
2024-11-20 23:39:04 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -131,9 +131,9 @@ def cartas():
         )
 
 # Handle tarot reading results
-@app.route('/results', methods=['POST'])
+@app.route('/results', methods=['GET','POST'])
 def results():
-    selected_cards_data = request.form.get('selected_cards_data')
+    selected_cards_data = request.form.get('selected_cards_data') or request.args.get('selected_cards_data') 
     choosed_cards = json.loads(selected_cards_data) if selected_cards_data else []
     intencao = session.get('intencao')
     # intencao = session_data['intencao']
