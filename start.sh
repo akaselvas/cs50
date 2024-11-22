0509edd6ee67b465a228598448f1520eb9fc9a30
selvas <91513384+akaselvas@users.noreply.github.com>
2024-11-21 22:09:08 -0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 # export PORT=${PORT:-10000}
 #!/bin/bash
 
-# Activate the virtual environment
-source .venv/bin/activate # Add this line to activate the venv
-
 # Monkey patch *before* anything else!
 # This needs to happen *within* the activated virtual environment.
 exec python -c "import eventlet; eventlet.monkey_patch()" # Use exec python -c
+
+
+# Activate the virtual environment
+source .venv/bin/activate # Add this line to activate the venv
+
 
 # Install dependencies (can be done before patching if preferred)
 pip install -r requirements.txt
